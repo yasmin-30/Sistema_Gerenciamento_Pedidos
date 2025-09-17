96d51acfda6e6de3c5420d5847e9baef68ded36c
yasmin-30 <161765927+yasmin-30@users.noreply.github.com>
2025-09-17 10:34:13 -0300

    Update Sistema.java
    Alterações nos casos do switch

--- Sistema.java
+++ Sistema.java
@@ -2,20 +2,20 @@ package gerenciamentoPedidos;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.Iterator;
 
 public class Sistema {
-//FAZER VERIFICAÇÃO PARA NUMEROS NEGATIVOS OU QUE MUITO GRANDES NÃO PRESENTES
+
 	public static void main(String[] args) {
 		int idPedido = 0;
 		boolean paradaPedido = false;
 		LinkedList<Pedido> listaPedidos = new LinkedList<Pedido>();
 		Scanner leitor = new Scanner(System.in);
-		System.out.println("Seja bem vindo ao restaurante------!");
+		System.out.println("---BEM-VINDO AO RESTAURANTE!---");
 		
 		while(!paradaPedido) {
-			System.out.println("Digite o número da ação que deseja realizar: ");
-			System.out.println("1-Adicionar pedido. 2-Remover pedido. 3-Listar pedidos., 4-Sair.");
+			System.out.println("");
+			System.out.println("---O QUE DESEJA FAZER?---");
+			System.out.println("1-Adicionar pedido. 2-Remover pedido. 3-Listar pedidos, 4-Sair.");
 			int acao = leitor.nextInt();
 			leitor.nextLine();
 			
@@ -30,7 +30,6 @@ public class Sistema {
 					System.out.println("Escreva o nome do cliente:");
 					nome = leitor.nextLine();
 					
-					// FAZER WHILE DOS ITENS DO PEDIDO
 					while(!paradaItens) {
 						Item novoItem = null;
 						String nomeItem;
@@ -44,59 +43,71 @@ public class Sistema {
 						novoItem= new Item(nomeItem, preco);
 						itens.add(novoItem);
 						
-						System.out.println("Deseja adicionar mais itens ao pedido? Y/N");
-						String resposta = leitor.nextLine();
-						if(resposta.equalsIgnoreCase("N")) {
-							paradaItens = true;
+						boolean verificacaoResp = false;
+						while(!verificacaoResp) {
+							System.out.println("Deseja adicionar mais itens ao pedido? Y/N");
+							String resposta = leitor.nextLine();
+							if(resposta.equalsIgnoreCase("N")) {
+								paradaItens = true;
+								verificacaoResp = true;
+							} else if(!resposta.equalsIgnoreCase("Y")) {
+								System.out.println("Responsta inválida, responda com Y ou N!");
+								verificacaoResp = false;
+							} else {
+								verificacaoResp = true;
+							}
 						}
 					}
 					
-					pedidoFinal = new Pedido(idPedido, nome, itens);			
+					pedidoFinal = new Pedido(idPedido, nome, itens);
+					pedidoFinal.resumoPedido();
 					listaPedidos.add(pedidoFinal);
 					break;
 				
-				case 2:
-					int removido = 0;
-					
+				case 2:	
+					int verificar = 0;
+					int tamPedidos = listaPedidos.size()-1;
 					System.out.println("Informe o id do pedido: ");
 					double idRemover = leitor.nextDouble();
+					
 		
-			        Iterator<Pedido> iterator = listaPedidos.iterator();
-			        while (iterator.hasNext()) {
-			            Pedido pedidoAtual = iterator.next();
-			            if (pedidoAtual.getId() == idRemover) {
-			                iterator.remove();
+			        for(Pedido remover : listaPedidos) {
+			            if (remover.getId() == idRemover) {
+			                listaPedidos.remove(remover);
 			                System.out.println("Pedido removido!");
+			                break;
 			            } else {
-			            	removido+=1;
+			            	verificar+=1;
 			            }
 			        }
 			        
-			        if(removido == listaPedidos.size()-1) {
+			        if(verificar == tamPedidos) {
 			        	System.out.println("ERRO! O pedido informado não está presente na lista!");
 			        }
 			        break;
 			        
 				case 3:
+					System.out.println("---LISTAGEM DOS PEDIDOS---");
 					for(int i=0; i < listaPedidos.size(); i++) {
 						ArrayList<Item> listaItens = new ArrayList<Item>();
 						Pedido imprimirPedido = listaPedidos.get(i);
 						listaItens = imprimirPedido.getItens();
 						
-						System.out.println("-------------------------");
-						System.out.println("Nome do cliente: " + imprimirPedido.getName() + ".");
-						System.out.println("Id do cliente: " + imprimirPedido.getId() + ".");
+						System.out.println("");
 						
+						System.out.println("Cliente: " + imprimirPedido.getName() + ".");
+						System.out.println("Nº do pedido: " + imprimirPedido.getId() + ".");
 						for(int j=0; j < listaItens.size(); j++) {
 							Item imprimirItem = listaItens.get(j);
-							System.out.println("Nome do item: " + imprimirItem.getNomeItem() + ".");
-							System.out.println("Preço do item: " + imprimirItem.getPreco() + ".");
+							System.out.println("Item: " + imprimirItem.getNomeItem() + ".");
+							System.out.println("Preço: " + imprimirItem.getPreco() + ".");
 						}
 					}
 					break;
 				case 4:
 					paradaPedido = true;
-					System.out.println("Pedidos realizados com sucesso, volte sempre!");
+					System.out.println("Pedidos realizados com sucesso!");
+					System.out.println("---VOLTE SEMPRE---");
 					break;
 			}        
 		}

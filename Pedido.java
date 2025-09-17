272e870d6896b9daf2fc1d761db539363d244891
yasmin-30 <161765927+yasmin-30@users.noreply.github.com>
2025-09-17 10:34:49 -0300

    Update Pedido.java
    Alterações no resumoPedido

--- Pedido.java
+++ Pedido.java
@@ -24,4 +24,20 @@ public class Pedido {
 	public ArrayList<Item> getItens() {
 		return itens;
 	}
+	
+	public void resumoPedido() {
+		double valorTotal = 0;
+		System.out.println("");
+		System.out.println("---RESUMO DO PEDIDO---");
+		System.out.println("Nome do cliente: " + this.nome + ".");
+		System.out.println("Nº do pedido: " + this.id + ".");
+		
+		for(int i = 0; i < itens.size(); i++) {
+			Item imprimirItem = itens.get(i);
+			System.out.println("Item: " + imprimirItem.getNomeItem() + ".");
+			valorTotal += imprimirItem.getPreco();
+		}
+		
+		System.out.println("Total: " + valorTotal + ".");
+	}
 }

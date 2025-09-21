63d48879acc9456437ee0a85ad0b355061451a1f
yasmin-30 <161765927+yasmin-30@users.noreply.github.com>
2025-09-21 20:14:54 -0300

    Update Pedido.java
    Atualização das saídas

--- Pedido.java
+++ Pedido.java
@@ -27,16 +27,27 @@ public class Pedido {
 	
 	public void resumoPedido() {
 		double valorTotal = 0;
+		
+		System.out.println("\n========================================");
+		System.out.println("               REST AURANTE               ");
+		System.out.println("========================================");
 		System.out.println("");
-		System.out.println("Nome do cliente: " + this.nome + ".");
-		System.out.println("Nº do pedido: " + this.id + ".");
+		System.out.println("Cliente: " + this.nome);
+		System.out.println("Pedido N°: " + this.id);
+		System.out.println("----------------------------------------");
+		System.out.println("Itens:");
+
 		
 		for(int i = 0; i < itens.size(); i++) {
 			Item imprimirItem = itens.get(i);
-			System.out.println("Item: " + imprimirItem.getNomeItem() + " - R$: " + imprimirItem.getPreco());
+			System.out.println("- " + imprimirItem.getNomeItem() + " R$ " + imprimirItem.getPreco());
 			valorTotal += imprimirItem.getPreco();
 		}
 		
-		System.out.println("O valor total do pedido é R$: " + valorTotal + ".");
+		System.out.println("----------------------------------------");
+		System.out.println("Total: R$ " + valorTotal);
+		System.out.println("========================================");
+		System.out.println("      Obrigado pela preferência! :)     ");
+		System.out.println("========================================\n");
 	}
 }
